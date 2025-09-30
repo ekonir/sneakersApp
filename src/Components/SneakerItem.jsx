fffd2bc0b6aa74e2b1d4f7ff2f8913cd81cf68c3
Lea PISANESCHI-RENAULT <lpisa@lmdsio.com>
2025-09-30 08:47:14 +0200

    Avis

--- src/Components/SneakerItem.jsx
+++ src/Components/SneakerItem.jsx
@@ -2,14 +2,18 @@ import '../styles/SneakerItem.css';
 import CustomerReview from './CustomerReview';
 
 function SneakerItem({ bestSeller,image, nom, marque, prix, style, esthetique, confort}){
-    const handleClickAvis = () =>{
-        alert("bouton cliqué");
+    const formatReview = (reviewType, scaleValue)=>{
+     const scaleType = reviewType === 'confort' ? '😌':'💖'
+     const icons = scaleType.repeat(scaleValue);
+     return `${reviewType}: ${icons} (${scaleValue}/5)`;
+    };
+    
+     const handleClickAvis = () =>{
+        const esthetismeText = formatReview('esthétisme',esthetique);
+        const confortText = formatReview('confort', confort);
+        alert(`Avis pour ${nom}: \n${esthetismeText}\n${confortText}`);
     };
 
-    //const formatRewiew = (rewiewType, scaleValue)
-    // const scaleType = reviewType === 'confort' ? '😌':'💖'
-    // const icons = scaleType.repeat(scaleValue);
-    // return '${scaleType}: ${icons} (${scaleValue}/5)';
 
     return (
         <div className={`sneaker-item ${bestSeller ? 'best-seller' : ''}`}>
@@ -28,8 +32,7 @@ function SneakerItem({ bestSeller,image, nom, marque, prix, style, esthetique, c
 
             <div className= "sneaker_rewiew">
                 <button onClick={handleClickAvis}>Voir les avis</button>
-                <CustomerReview  reviewType='esthetique' scaleValue={esthetique}/>
-                <CustomerReview  reviewType='confort' scaleValue={confort}/>
+                
             </div>
         </div>
     );
